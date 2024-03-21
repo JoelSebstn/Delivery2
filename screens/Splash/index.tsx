3a68c98e4053322500627f6012c259877d5b61ac
Joel Sebastian <53466652+JoelSebstn@users.noreply.github.com>
2024-03-21 10:56:40 +0530

    Initial Setup
    Added blank Splash screen & navigations

---
+++ screens/Splash/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export function Splash() {
+    return <View>
+        <Text>Splash
+
+        </Text>
+    </View>
+}
---
+++ screens/navigatorRouter.tsx
@@ -0,0 +1,12 @@
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+export function NavigatorRouter() {
+    // return <NavigationContainer>
+    //     <Stack.Navigator initialRouteName="Splash">
+    //         <Stack.Screen name="Splash" component={Splash} options={{ headerShown: false }} />
+    //         {/* <Stack.Screen name="DetailsSCene" component={DetailsScene} options={{ title: 'Station Details' }} /> */}
+    //     </Stack.Navigator>
+    // </NavigationContainer>
+}
